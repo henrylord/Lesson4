65f4e0328daecd0ff112b3588d5526694c4f84b3
hhong <hhong@logixits.com>
2024-01-01 14:23:17 -0500

    #commit Lesson#4 code ## 1. Overloading Methods ### 2. Overloading Constructors ### 3. Using Objects as Parameters        Pass by Value

--- src/main/java/org/example/PassByValue.java
+++ src/main/java/org/example/PassByValue.java
@@ -2,9 +2,27 @@ package org.example;
 
 public class PassByValue {
 
+    int num;
 
 
+    PassByValue(int a){
 
+        this.num = a;
+    }
 
+    public void doubleValue(int a){
+
+        a = a *2;
+
+    }
+
+    public static void main(String[] args) {
+
+        PassByValue test1 = new PassByValue(200);
+
+        test1.doubleValue(test1.num);
+
+        System.out.println(test1.num);
+    }
 
 }
